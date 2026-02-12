bc8f5d7ef7b9506609f6074bd3e1d0f451d70bc0
Vasyl Kovalov <raid3r81@gmail.com>
2026-02-12 19:47:52 +0200

    Functions

---
+++ AcademyP43/Functions/BirthdayText.sql
@@ -0,0 +1,35 @@
+﻿CREATE FUNCTION [dbo].[BirthdayText]
+(
+	@birthdayDate DATETIME
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN -- {
+	DECLARE @currentDate DATETIME = GETDATE();
+	DECLARE @currentMonth INT = MONTH(@currentDate);
+	DECLARE @currentDay INT = DAY(@currentDate);
+	DECLARE @birthdayMonth INT = MONTH(@birthdayDate);
+	DECLARE @birthdayDay INT = DAY(@birthdayDate);
+	DECLARE @diffInDays INT = 5;
+	
+	IF @currentMonth = @birthdayMonth AND @currentDay = @birthdayDay
+	BEGIN -- {
+		RETURN N'Сьогодні день народження!';
+	END -- }
+
+	DECLARE @birthdayInThisYear DATETIME = DATEFROMPARTS(YEAR(@currentDate), @birthdayMonth, @birthdayDay);
+
+	IF DATEDIFF(DAY, @currentDate, @birthdayInThisYear) < (-1 * @diffInDays)
+		RETURN N'День народження не сьогодні.';
+
+	IF DATEDIFF(DAY, @currentDate, @birthdayInThisYear) < @diffInDays AND DATEDIFF(DAY, @currentDate, @birthdayInThisYear) >= 0
+		RETURN N'День народження скоро!';
+	
+	IF DATEDIFF(DAY, @currentDate, @birthdayInThisYear) > (-1 * @diffInDays) AND DATEDIFF(DAY, @currentDate, @birthdayInThisYear) < 0
+		RETURN N'День народження було нещодавно!';
+		
+	
+	RETURN N'День народження не сьогодні.';
+
+
+END -- }
---
+++ AcademyP43/FunctionsExamples.sql
@@ -0,0 +1,78 @@
+﻿
+
+
+
+SELECT * FROM Teachers;
+
+SELECT GETDATE() AS CurrentDateTime;
+
+SELECT 
+MONTH(GETDATE()) AS CurrentMonth,
+YEAR(GETDATE()) AS CurrentYear,
+DAY(GETDATE()) AS CurrentDay,
+DATEPART(HOUR, GETDATE()) AS CurrentHour,
+DATEPART(MINUTE, GETDATE()) AS CurrentMinute,
+DATEPART(SECOND, GETDATE()) AS CurrentSecond;
+
+
+SELECT DATEPART(HOUR, '2022-01-01 18:34:56') AS DateTimeHour;
+
+
+SELECT * FROM Teachers
+WHERE DATEPART(MONTH, EmploymentDate) = DATEPART(MONTH, GETDATE());	
+
+
+SELECT 
+Name,
+Surname,
+EmploymentDate,
+dbo.BirthdayText(EmploymentDate) AS BirthdayText
+FROM Teachers;
+
+
+DECLARE @currentDate DATETIME = GETDATE();
+DECLARE @birthdayDay INT = 15;
+DECLARE @birthdayMonth INT = 02;
+DECLARE @birthdayInThisYear DATETIME = DATEFROMPARTS(YEAR(@currentDate), @birthdayMonth, @birthdayDay);
+PRINT 'DIFF IN DAYS: ' + CAST(DATEDIFF(DAY, @currentDate, @birthdayInThisYear) AS NVARCHAR);
+
+
+
+DECLARE @departmentId INT = 2;
+SELECT * FROM Teachers WHERE DepartmentId = @departmentId;
+
+
+
+
+
+
+UPDATE Teachers
+SET EmploymentDate = DATEADD(DAY, -3, GETDATE())
+WHERE Id = 1;
+UPDATE Teachers
+SET EmploymentDate = DATEADD(DAY, 3, GETDATE())
+WHERE Id = 2;
+UPDATE Teachers
+SET EmploymentDate = GETDATE()
+WHERE Id = 3;
+
+
+
+SELECT DATEADD(MONTH, -1, GETDATE()) AS PreviousMonth;
+
+SELECT DATEADD(HOUR, -3, GETDATE()) AS Previous3Hour;
+
+
+
+
+
+
+
+
+
+/*
+
+-- HOUR(GETDATE()) AS CurrentHour,
+-- MINUTE(GETDATE()) AS CurrentMinute,
+-- SECOND(GETDATE()) AS CurrentSecond
+*/
